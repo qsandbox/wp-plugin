bf676ae989324b1c058526d9ecc11cd1e19e7ec0
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2016-01-06 17:17:18 +0200

    added result obj

---
+++ lib/result.php
@@ -0,0 +1,56 @@
+<?php
+
+class qSandbox_Result {
+    public $msg = '';
+    public $status = 0;
+    public $data = array();
+
+    public function isSuccess() {
+        return ! empty( $this->status );
+    }
+
+    public function isError() {
+        return ! $this->isSuccess();
+    }
+
+    /**
+     * Getter/setting for status
+     * @param bool $status
+     * @return bool
+     */
+    public function status($status = null) {
+        if (!is_null($status)) {
+            $this->status = $status;
+        }
+
+        return $this->status;
+    }
+
+    public function msg($msg = null) {
+        if (!is_null($msg)) {
+            $this->msg = $msg;
+        }
+
+        return $this->msg;
+    }
+
+    public function data($records = null) {
+        if (!is_null($records)) {
+            $this->data = $records;
+        }
+
+        return $this->data;
+    }
+
+    public function __set($name, $value) {
+        $this->$name = $value;
+    }
+
+    public function __get($name) {
+        return isset($this->$name) ? $this->$name : null;
+    }
+
+    public function __call($name, $arguments) {
+        
+    }
+}
