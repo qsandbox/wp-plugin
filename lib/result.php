44deed3548b434b136782337e325fd078a6b1ed6
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2016-01-07 12:04:23 +0200

    updated data method to accept various data

--- lib/result.php
+++ lib/result.php
@@ -5,10 +5,18 @@ class qSandbox_Result {
     public $status = 0;
     public $data = array();
 
+    /**
+     * Cool method which is nicer than checking for a status value.
+     * @return bool
+     */
     public function isSuccess() {
         return ! empty( $this->status );
     }
 
+    /**
+     * Cool method which is nicer than checking for a status value.
+     * @return bool
+     */
     public function isError() {
         return ! $this->isSuccess();
     }
@@ -34,14 +42,20 @@ class qSandbox_Result {
         return $this->msg;
     }
 
-    public function data($records = null) {
-        if ( ! empty( $value ) ) {
-            $this->data[ $key_or_records ] = $value;
-        } elseif ( ! is_null( $records ) ) {
-            $this->data = $records;
+    public function data($key = '', $val = null) {
+        if (is_array($key)) { // when we pass an array -> override all
+            $this->data = empty($this->data) ? $key : array_merge($this->data, $key);
+        } elseif (!empty($key)) {
+            if (!is_null($val)) { // add/update a value
+                $this->data[$key] = $val;
+            }
+
+            return isset($this->data[$key]) ? $this->data[$key] : null;
+        } else { // nothing return all data
+            $val = $this->data;
         }
 
-        return $this->data;
+        return $val;
     }
 
     public function __set($name, $value) {
